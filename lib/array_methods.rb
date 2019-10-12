11ffb10165140413ee846092cd09f3b7c3765293
Nory Lazo <nlazo@icloud.com>
2019-10-12 00:09:08 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,20 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end 
+  nil
+end 
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min 
 end
+
+positives = [2, 4, 6, 8, 10]
+
+find_element_index(positives, 2)
